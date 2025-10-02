10e44007efa5629a35ee97d3ef2c8e2f806ed60d
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-02 10:53:04 +0300

    add tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,20 @@
+import pytest
+from src.calculator import Calculator
+tests = [
+    ('3 2 4 ** +', 19),
+    ('5 1 2 + 4 ** + 3 -', 83),
+    ('10 3 // 2 *', 6),
+    ('20 3 % 2 +', 4),
+    ('2 3 + 4 *', 20),
+    ('2 3 4 * +', 14),
+    ('2 +2 +', 4),
+    ('5 -78 +', -73),
+    ('3 ( 2 1 + ) *', 9),
+    ('6 ( 2 3 ** ) /', 0.75),
+]
+
+calc = Calculator()
+
+@pytest.mark.parametrize('expr,expected', tests)
+def test_calculator(expr, expected):
+    assert calc.calculate_rpn(expr) == pytest.approx(expected)
