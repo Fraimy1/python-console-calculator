e6871bcbdc87bf27deb9d36a205f541ad20fc452
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-10-11 01:45:47 +0300

    console interface tweaks

--- src/main.py
+++ src/main.py
@@ -3,20 +3,37 @@ from calculator import Calculator
 user_welcome_message = """
 =============================================
 Welcome to the RPN calculator!
+
 You can insert any RPN expression and the calculator will return the result.
-e.g. "6 2 +" -> 8 or something crazy like "( 3 (4        (2)   -) ** )" 
-it can handle it (probably)
+e.g. "6 2 +" -> 8.0 or something crazy like "( 3 (4        (2)   -) ** )" -> 9.0
+Calculator can (probably) handle it! 
+
 Press Ctrl + C or type in "exit" to exit
 =============================================
+"""
+
+user_goodbye_message = """
+================
+
+Goodbye!
+
+================
+
 """
 
 if __name__ == '__main__':
     calc = Calculator()
     print(user_welcome_message)
     while True:
-        expr = input("Insert your expression: ")
-        if expr == "exit":
+        try: 
+            expr = input("Insert your expression: ")
+        except: 
+            print('\n', user_goodbye_message)
             break
+        if expr == "exit":
+            print(user_goodbye_message)
+            break
+        
         try: 
             result = calc.calculate_rpn(expr)
             print(f'Result: {result}\n')
