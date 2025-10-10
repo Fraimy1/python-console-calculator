dd7a1ba91c59248c5e1dbe43592de1b00d8aabf6
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-10-11 01:00:10 +0300

    Add more error handlers and other minor fixes

--- src/calculator.py
+++ src/calculator.py
@@ -1,31 +1,32 @@
 import re
-import operator
 
 NUM = r"[+-]?[\d_]+(?:\.[\d_]+)?"
 
 pattern = rf"""
     \s*
     (
-        \*\*            |   # двойная *
-        //              |   # двойной /
-        %               |   # одиночный %
-        \*(?!\*)        |   # одиночная *
-        /(?!/)          |   # одиночный /
-        {NUM}            |   # число с возможным знаком
-        [*/()%]          |   # одиночные токены кроме + -
-        [+\-]                # + или - как отдельный оператор
+        \*\*            |   # Double *
+        //              |   # Double /
+        %               |   # solo %
+        \*(?!\*)        |   # solo *
+        /(?!/)          |   # solo /
+        {NUM}           |   # Number
+        [*/()%]         |   # tokens except + -
+        [+\-]               # + or - as separate operators
     )
 """
 
 TOKEN_RE2 = re.compile(pattern, re.VERBOSE)
 
 class Parser:
+    """Covnerts str RPN expression into tokens"""
     def __init__(self):
         self.smth = None
 
     def parse(self, expr: str):
+        """Parses str RPN expression into tokens using re pattern"""
+
         expr = TOKEN_RE2.findall(expr)
-        print('parsed expression:', expr)
 
         parsed = []
         for term in expr:
@@ -45,25 +46,28 @@ class CalcError(Exception):
     pass
 
 class Calculator:
-    def __init__(self):
+    """Calculates RPN expressions using expression parsed by Parser.parse()"""
+    def __init__(self) -> None:
         self.parser = Parser()
 
     def calculate_rpn(self, expr: str) -> float:
+        """Calculates RPN and returns result"""
+        
         if not expr or not expr.strip():
             raise CalcError("Пустой ввод")
         
         expr = self.parser.parse(expr)
 
-        print(f'{expr=}')
         st: list[float] = []
         
-        print(expr)
+        paren_marks = [] # marks the point where parentheses open
+
         for tok_type, value in expr:
             if tok_type == 'OP':
                 if len(st) < 2:
                     raise CalcError("Недостаточно операндов")
-                b = st.pop()       # второе число
-                a = st.pop()       # первое число
+                b = st.pop()      
+                a = st.pop()       
                 if value == "+": st.append(a + b)
                 elif value == "-": st.append(a - b)
                 elif value == "*": st.append(a * b)
@@ -78,15 +82,27 @@ class Calculator:
                         raise CalcError("Деление на ноль")
                     st.append(a / b)
             elif tok_type == 'PAR':
-                ...
+                if value == '(':
+                    paren_marks.append(len(st))
+                else:
+                    if not paren_marks:
+                        raise CalcError('The parentheses were never opened')
+                    paren_size = len(st) - paren_marks.pop()
+                    if paren_size != 1:
+                        raise CalcError(f'Incorrect parentheses expression. Expected format is (2) or (2 1 +)')
+                    # opened_par_count -= 1
             elif tok_type == 'NUM':
                 try:
                     st.append(float(value))
                 except ValueError:
-                    raise CalcError(f"Not число и не оператор: {value}")
-
+                    raise CalcError(f"Not a number or operator: {value}")
+            
         if len(st) != 1:
             raise CalcError("Лишние данные в выражении")
+        
+        if paren_marks:
+            raise CalcError(f"Mismatched parentheses: {len(paren_marks)} '(' were never closed")
+
         return st[0]
 
 if __name__ == '__main__':
@@ -104,5 +120,4 @@ if __name__ == '__main__':
     ('3 2 1 + *', 9),
     ('6 2 3 ** /', 0.75),
 ]
-    print(calc.calculate_rpn('1_000_000.132_123 2 +'), 2**(2**3), (2**2)**3)
-    # print(1.2_2_3)
+    print(calc.calculate_rpn('(3 (1_000_000.132_123 2 +) *)'))
