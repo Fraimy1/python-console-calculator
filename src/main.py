578f7eb4276a88d9254c511792820da90b889aef
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-21 16:07:39 +0300

    Remove unnecessary check

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from src.calculator import Calculator
 from src.constants import USER_WELCOME_MESSAGE, USER_GOODBYE_MESSAGE
-from src.errors import CalcError
 
 def main():
     calc = Calculator()
@@ -16,8 +15,6 @@ def main():
             break
 
         try:
-            if not expr or not expr.strip():
-                raise CalcError("Empty expression")
             result = calc.calculate_rpn(expr)
             print(f"Result: {result}\n")
         except Exception as e:
